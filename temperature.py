64de72f36768c24cbc631d1bd139ca4171068e75
Ubuntu <ubuntu@ip-172-31-20-243.ec2.internal>
2019-11-15 23:59:59 +0000

    2

--- temperature.py
+++ temperature.py
@@ -9,16 +9,17 @@ def fahrenheit():
     # calculates temperature from celsius to fahrenheit
 
     # input
-    print("Let's convert temperatures from celsius to fahrenheit! ")
+    print("Let's convert temperatures from celsius to fahrenheit!")
     temp_in_celsius_string = input("What is the temperature? (°C): ")
     print("")
 
     # process
     try:
-        temp_in_celsius = int(temp_in_celsius_string)
+        temp_in_celsius = float(temp_in_celsius_string)
         temp_in_fahrenheit = ((9/5) * temp_in_celsius + 32)
-        print("The temperature is {0}°F in fahrenheit"
-              "."format(temp_in_fahrenheit))
+        # output
+        print("The temperature is {0}°F in fahrenheit."
+              .format(temp_in_fahrenheit))
     except Exception:
         print("Wrong input. Try again.")
 
